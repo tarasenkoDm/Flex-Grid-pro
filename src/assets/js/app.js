f10a5e221aaceabbd902077b82801ae6c6cb50f2
tarasenkoDm <lordtda@gmail.com>
2017-03-27 23:58:27 +0300

    5 blocks - first step

---
+++ src/assets/js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $('.portfolio-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true
+    });
+
+    $('.header-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        accessibility: false,
+        speed: 500,
+        fade: true
+    });
+});
+
+$(document).ready(function () {
+    $('.examples-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        accessibility: false,
+        speed: 500,
+        fade: true
+    });
+});
